f1f29b16a3968e6a8b3785c8c32f2612e0cc5b61
Sergio Camacho Toledano <sergio.camacho.soporte@gmail.com>
2025-01-12 19:17:09 +0100

    feat: modified challenge 1 script

--- scripts/install-challenge-1.sh
+++ scripts/install-challenge-1.sh
@@ -9,10 +9,12 @@ echo "Installing random service..."
 helm install nginx ./challenges/challenge-1/random-service/ -f ./challenges/challenge-1/random-service/values.yaml -n applications
 echo "Random service installed!"
 echo "Checking service status..."
+echo "-----------------------------------------------------"
 kubectl get pods -n applications
+echo "-----------------------------------------------------"
 echo "Installing ping service..."
 helm install ping ./challenges/challenge-1/ping/ -f ./challenges/challenge-1/ping/values.yaml -n applications
 echo "Ping service installed!"
-echo "Waiting 5 seconds for the service to be up" && sleep 5
 echo "Checking service status..."
-kubectl get pods -n applications -o wide
+echo "-----------------------------------------------------"
+kubectl get pods -n applications -o wide -w
